371372d930a7be7749f0dd504d6abd35db725090
Vince Jerald Villamora <vincevillamora@gmail.com>
2023-09-24 16:06:18 +0800

    Add error handling

--- an5506_stock.go
+++ an5506_stock.go
@@ -25,25 +25,58 @@ func (o AN5506_Stock) UpdateCachedPage() {
 	jar, err := cookiejar.New(&options)
 	if err != nil {
 		log.Fatal(err)
+		cachedPage.SetPage(nil)
+		cachedPage2.SetPage(nil)
+		return
 	}
-	client := http.Client{Jar: jar}
 
+	client := http.Client{Jar: jar}
 	form := url.Values{}
 	form.Add("User", "user")          // webgui username
 	form.Add("Passwd", "tattoo@home") // webgui password
-	req, _ := http.NewRequest("POST", o.GetGponUrl()+"/goform/webLogin", strings.NewReader(form.Encode()))
-	req.Header.Set("Referer", "http://127.0.0.1/gpon-parser") // must define a referer or it will fail
-	_, err = client.Do(req)
+
+	// 1. Login to UI
+	req, err := http.NewRequest("POST", o.GetGponUrl()+"/goform/webLogin", strings.NewReader(form.Encode()))
 	if err != nil {
 		log.Fatal(err)
+		cachedPage.SetPage(nil)
+		cachedPage2.SetPage(nil)
+		return
 	}
 
+	req.Header.Set("Referer", "http://127.0.0.1/gpon-parser") // must define a referer or it will fail
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		cachedPage.SetPage(nil)
+		cachedPage2.SetPage(nil)
+		return
+	}
+	defer resp.Body.Close()
+
+	// 2. Get optical power
 	parsePage(client, gponSvc.GetGponUrl()+"/state/opt_power.asp", cachedPage)
+
+	// 3. Get device information
 	parsePage(client, gponSvc.GetGponUrl()+"/state/deviceInfor.asp", cachedPage2)
 
-	req, _ = http.NewRequest("GET", o.GetGponUrl()+"/goform/webLogout", nil)
+	// 4. Logout from UI
+	req, err = http.NewRequest("GET", o.GetGponUrl()+"/goform/webLogout", nil)
+	if err != nil {
+		log.Fatal(err)
+		cachedPage.SetPage(nil)
+		cachedPage2.SetPage(nil)
+		return
+	}
+
 	req.Header.Set("Referer", "http://192.168.254.254/menu_ph_globe.asp") // must define a referer to logout cleanly
-	client.Do(req)
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer resp.Body.Close()
 }
 
 func parsePage(client http.Client, url string, docPage *DocPage) {
@@ -62,39 +95,50 @@ func parsePage(client http.Client, url string, docPage *DocPage) {
 		log.Fatal(err)
 		docPage.SetPage(nil)
 	}
+
+	defer resp.Body.Close()
 }
 
 func (o AN5506_Stock) GetOpticalInfo() *OpticalStats {
-	parsedList := make([]string, 0, 5)
-	for i := 2; i < 7; i++ {
-		htmlNode := htmlquery.FindOne(cachedPage.GetPage(), fmt.Sprintf("/html/body/form/table[3]/tbody/tr[%d]/td[2]", i))
-		parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
-	}
+	var opticalInfo *OpticalStats
 
-	var opticalInfo = new(OpticalStats)
-	opticalInfo.TxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[0], 4), 64)
-	opticalInfo.RxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[1], 4), 64)
-	opticalInfo.Temperature, _ = strconv.ParseFloat(removeLastNChars(parsedList[2], 4), 64)
-	opticalInfo.SupplyVoltage, _ = strconv.ParseFloat(removeLastNChars(parsedList[3], 2), 64)
-	opticalInfo.BiasCurrent, _ = strconv.ParseFloat(removeLastNChars(parsedList[4], 3), 64)
+	if cachedPage.GetPage() != nil {
+		opticalInfo = new(OpticalStats)
+
+		parsedList := make([]string, 0, 5)
+		for i := 2; i < 7; i++ {
+			htmlNode := htmlquery.FindOne(cachedPage.GetPage(), fmt.Sprintf("/html/body/form/table[3]/tbody/tr[%d]/td[2]", i))
+			parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
+		}
+		opticalInfo.TxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[0], 4), 64)
+		opticalInfo.RxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[1], 4), 64)
+		opticalInfo.Temperature, _ = strconv.ParseFloat(removeLastNChars(parsedList[2], 4), 64)
+		opticalInfo.SupplyVoltage, _ = strconv.ParseFloat(removeLastNChars(parsedList[3], 2), 64)
+		opticalInfo.BiasCurrent, _ = strconv.ParseFloat(removeLastNChars(parsedList[4], 3), 64)
+	}
 
 	return opticalInfo
 }
 
 func (o AN5506_Stock) GetDeviceInfo() *DeviceStats {
-	parsedList := make([]string, 0, 6)
-	for i := 2; i < 16; i++ {
-		htmlNode := htmlquery.FindOne(cachedPage2.GetPage(), fmt.Sprintf("/html/body/form/table[3]/tbody/tr[%d]/td[2]", i))
-		parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
-	}
+	var deviceInfo *DeviceStats
 
-	var deviceInfo = new(DeviceStats)
-	deviceInfo.DeviceModel = parsedList[2]
-	deviceInfo.ModelSerial = strings.ReplaceAll(parsedList[4], "FFFFFF", "")
-	deviceInfo.SoftwareVersion = parsedList[0]
-	deviceInfo.MemoryUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[10], 1), 64)
-	deviceInfo.CpuUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[9], 1), 64)
-	deviceInfo.Uptime = parseDuration(parsedList[13])
+	if cachedPage2.GetPage() != nil {
+		deviceInfo = new(DeviceStats)
+
+		parsedList := make([]string, 0, 6)
+		for i := 2; i < 16; i++ {
+			htmlNode := htmlquery.FindOne(cachedPage2.GetPage(), fmt.Sprintf("/html/body/form/table[3]/tbody/tr[%d]/td[2]", i))
+			parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
+		}
+
+		deviceInfo.DeviceModel = parsedList[2]
+		deviceInfo.ModelSerial = strings.ReplaceAll(parsedList[4], "FFFFFF", "")
+		deviceInfo.SoftwareVersion = parsedList[0]
+		deviceInfo.MemoryUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[10], 1), 64)
+		deviceInfo.CpuUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[9], 1), 64)
+		deviceInfo.Uptime = parseDuration(parsedList[13])
+	}
 
 	return deviceInfo
 }
--- hg6245d_globe.go
+++ hg6245d_globe.go
@@ -22,36 +22,44 @@ func (o HG6245D_Globe) UpdateCachedPage() {
 }
 
 func (o HG6245D_Globe) GetOpticalInfo() *OpticalStats {
-	parsedList := make([]string, 0, 5)
-	for i := 1; i < 6; i++ {
-		htmlNode := htmlquery.FindOne(cachedPage.GetPage(), fmt.Sprintf("/html/body/div[1]/div[1]/div[2]/div/div[4]/ul/li[%d]/span[2]", i))
-		parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
-	}
+	var opticalInfo *OpticalStats
 
-	var opticalInfo = new(OpticalStats)
-	opticalInfo.TxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[0], 4), 64)
-	opticalInfo.RxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[1], 4), 64)
-	opticalInfo.Temperature, _ = strconv.ParseFloat(removeLastNChars(parsedList[2], 4), 64)
-	opticalInfo.SupplyVoltage, _ = strconv.ParseFloat(removeLastNChars(parsedList[3], 2), 64)
-	opticalInfo.BiasCurrent, _ = strconv.ParseFloat(removeLastNChars(parsedList[4], 3), 64)
+	if cachedPage.GetPage() != nil {
+		opticalInfo = new(OpticalStats)
+		parsedList := make([]string, 0, 5)
+		for i := 1; i < 6; i++ {
+			htmlNode := htmlquery.FindOne(cachedPage.GetPage(), fmt.Sprintf("/html/body/div[1]/div[1]/div[2]/div/div[4]/ul/li[%d]/span[2]", i))
+			parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
+		}
+
+		opticalInfo.TxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[0], 4), 64)
+		opticalInfo.RxPower, _ = strconv.ParseFloat(removeLastNChars(parsedList[1], 4), 64)
+		opticalInfo.Temperature, _ = strconv.ParseFloat(removeLastNChars(parsedList[2], 4), 64)
+		opticalInfo.SupplyVoltage, _ = strconv.ParseFloat(removeLastNChars(parsedList[3], 2), 64)
+		opticalInfo.BiasCurrent, _ = strconv.ParseFloat(removeLastNChars(parsedList[4], 3), 64)
+	}
 
 	return opticalInfo
 }
 
 func (o HG6245D_Globe) GetDeviceInfo() *DeviceStats {
-	parsedList := make([]string, 0, 6)
-	for i := 1; i < 7; i++ {
-		htmlNode := htmlquery.FindOne(cachedPage.GetPage(), fmt.Sprintf("/html/body/div[1]/div[1]/div[2]/div/div[5]/ul/li[%d]/span[2]", i))
-		parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
-	}
+	var deviceInfo *DeviceStats
 
-	var deviceInfo = new(DeviceStats)
-	deviceInfo.DeviceModel = parsedList[0]
-	deviceInfo.ModelSerial = parsedList[1]
-	deviceInfo.SoftwareVersion = parsedList[2]
-	deviceInfo.MemoryUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[3], 1), 64)
-	deviceInfo.CpuUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[4], 1), 64)
-	deviceInfo.Uptime = parseDuration(parsedList[5])
+	if cachedPage.GetPage() != nil {
+		deviceInfo = new(DeviceStats)
+		parsedList := make([]string, 0, 6)
+		for i := 1; i < 7; i++ {
+			htmlNode := htmlquery.FindOne(cachedPage.GetPage(), fmt.Sprintf("/html/body/div[1]/div[1]/div[2]/div/div[5]/ul/li[%d]/span[2]", i))
+			parsedList = append(parsedList, normalizeString(htmlquery.InnerText(htmlNode)))
+		}
+
+		deviceInfo.DeviceModel = parsedList[0]
+		deviceInfo.ModelSerial = parsedList[1]
+		deviceInfo.SoftwareVersion = parsedList[2]
+		deviceInfo.MemoryUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[3], 1), 64)
+		deviceInfo.CpuUsage, _ = strconv.ParseFloat(removeLastNChars(parsedList[4], 1), 64)
+		deviceInfo.Uptime = parseDuration(parsedList[5])
+	}
 
 	return deviceInfo
 }
