2b23a724248759619874699f6f65b43891bc6d40
Vince JV <1276544+vincejv@users.noreply.github.com>
2024-10-23 14:33:34 +0800

    Handle device error gracefully

--- controller.go
+++ controller.go
@@ -10,23 +10,23 @@ import (
 
 func servOpticalInfo(c *gin.Context) {
 	stat := gponSvc.GetOpticalInfo()
-	if stat.Temperature > 0.0 && stat.RxPower < -5.0 && stat.TxPower > 0.1 {
+	if stat != nil && stat.Temperature > 0.0 && stat.RxPower < -5.0 && stat.TxPower > 0.1 {
 		c.JSON(http.StatusOK, stat)
 	} else {
-		log.Println("Unable to fetch gpon optical stats at the moment, returning http 403")
+		log.Println("Unable to fetch gpon optical stats at the moment, returning http 500")
 		log.Printf("servOpticalInfo: %+v\n", stat)
-		c.JSON(http.StatusForbidden, nil)
+		c.JSON(http.StatusInternalServerError, nil)
 	}
 }
 
 func servDeviceInfo(c *gin.Context) {
 	stat := gponSvc.GetDeviceInfo()
-	if len(strings.TrimSpace(stat.DeviceModel)) >= 0 {
+	if stat != nil && len(strings.TrimSpace(stat.DeviceModel)) >= 0 {
 		c.JSON(http.StatusOK, gponSvc.GetDeviceInfo())
 	} else {
-		log.Println("Unable to fetch gpon device stats at the moment, returning http 403")
+		log.Println("Unable to fetch gpon device stats at the moment, returning http 500")
 		log.Printf("servDeviceInfo: %+v\n", stat)
-		c.JSON(http.StatusForbidden, nil)
+		c.JSON(http.StatusInternalServerError, nil)
 	}
 }
 
@@ -36,11 +36,12 @@ func servAllInfo(c *gin.Context) {
 	allInfo.OpticalStats = gponSvc.GetOpticalInfo()
 	allInfo.DeviceStats = gponSvc.GetDeviceInfo()
 
-	if allInfo.OpticalStats.Temperature > 0.0 && len(strings.TrimSpace(allInfo.DeviceStats.DeviceModel)) >= 0 {
+	if allInfo.OpticalStats != nil && allInfo.DeviceStats != nil &&
+		allInfo.OpticalStats.Temperature > 0.0 && len(strings.TrimSpace(allInfo.DeviceStats.DeviceModel)) >= 0 {
 		c.JSON(http.StatusOK, allInfo)
 	} else {
-		log.Println("Unable to fetch all gpon stats at the moment, returning http 403")
+		log.Println("Unable to fetch all gpon stats at the moment, returning http 500")
 		log.Printf("servAllInfo: %+v\n", allInfo)
-		c.JSON(http.StatusForbidden, nil)
+		c.JSON(http.StatusInternalServerError, nil)
 	}
 }
